b8e497680eba7fed03e94256407938b3a98ac520
PatrickCaproni <patrick.-m@hotmail.com>
2023-12-31 10:17:35 -0300

    set categories in products to FetchType.EAGER

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Product.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Product.java
@@ -43,7 +43,7 @@ public class Product implements Serializable {
     @Column(name = "img_url")
     private String imgUrl;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "tb_product_category",
             joinColumns = @JoinColumn(name = "product_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id")
---
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/ProductResources.java
@@ -0,0 +1,73 @@
+package com.devsuperior.dscatalog.resources;
+
+import com.devsuperior.dscatalog.dtos.ProductDTO;
+import com.devsuperior.dscatalog.entities.Product;
+import com.devsuperior.dscatalog.mappers.ProductMapper;
+import com.devsuperior.dscatalog.services.ProductService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.net.URI;
+
+@Slf4j
+@RestController
+@RequestMapping(value = "/products")
+public class ProductResources implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Autowired
+    private ProductService service;
+
+    @Autowired
+    private ProductMapper mapper;
+
+    @GetMapping
+    public ResponseEntity<Page<ProductDTO>> findAllPaged(@RequestParam(value = "page", defaultValue = "0") Integer page,
+                                                         @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+                                                         @RequestParam(value = "direction", defaultValue = "ASC") String direction,
+                                                         @RequestParam(value = "orderBy", defaultValue = "name") String orderBy) {
+        log.info("[Products Controller] - List all products paged");
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        Page<Product> products = service.findAllPaged(pageRequest);
+        return ResponseEntity.ok().body(products.map(product -> mapper.toDto(product)));
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
+        log.info("[Products Controller] - Get product by id");
+        Product product = service.findById(id);
+        return ResponseEntity.ok().body(mapper.toDto(product));
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO product) {
+        log.info("[Products Controller] - Register new Product");
+        Product newProduct = service.insert(mapper.toEntity(product));
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(product.getId()).toUri();
+        return ResponseEntity.created(uri).body(mapper.toDto(newProduct));
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO product) {
+        log.info("[Products Controller] - Update product by id");
+        Product newProduct = service.update(id, product);
+        return ResponseEntity.ok().body(mapper.toDto(newProduct));
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        log.info("[Products Controller] - Delete product by id");
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
