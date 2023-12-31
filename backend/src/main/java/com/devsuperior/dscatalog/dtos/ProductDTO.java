dd1d1a7a4503cab3d8a77f5a268789c15615882a
PatrickCaproni <patrick.-m@hotmail.com>
2023-12-31 09:35:24 -0300

    create product DTO

--- backend/src/main/java/com/devsuperior/dscatalog/dtos/ProductDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dtos/ProductDTO.java
@@ -1,10 +1,14 @@
 package com.devsuperior.dscatalog.dtos;
 
+import com.devsuperior.dscatalog.entities.Category;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -12,10 +16,15 @@ import java.io.Serializable;
 @Setter
 @JsonIgnoreProperties(ignoreUnknown = true)
 @ToString
-public class CategoryDTO implements Serializable {
+public class ProductDTO implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
-    private Long Id;
+    private Long id;
     private String name;
+    private Double price;
+    private Instant date;
+    private String description;
+    private String imgUrl;
+    private List<Category> categories = new ArrayList<>();
 }
